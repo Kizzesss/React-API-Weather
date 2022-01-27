52ecf86f2995e71b1970893531dcc4e1b38cb631
Kizzesss <73948212+Kizzesss@users.noreply.github.com>
2022-01-27 13:28:13 -0500

    Guardar datos seleccionados de la API

--- src/app/app.js
+++ src/app/app.js
@@ -6,7 +6,15 @@ import {API_WEATHER_KEY} from "./keys";
 
 class App extends Component {
 
-    
+    state = {
+        temperature: '',
+        description: '',
+        humidity: '',
+        wind: '',
+        city: '',
+        country: '',
+        error: null
+    };
 
     getWeather = async e => {
         e.preventDefault();
@@ -17,9 +25,17 @@ class App extends Component {
         const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${API_WEATHER_KEY}&units=metric`;
         const response = await fetch(API_URL);
         const data = await response.json();
-        console.log(data);
+        console.log(this.state);
 
-        console.log(cityValue, countryValue);
+        this.setState({
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            humidity: data.main.humidity,
+            wind: data.wind.speed,
+            city: data.name,
+            country: data.sys.country,
+            error: null
+        }, () => console.log(this.state));
     }
   
     render() {
@@ -28,7 +44,7 @@ class App extends Component {
             <div className="row">
                 <div className="col-md-4 mx-auto">
                     <WeatherForm getWeather={this.getWeather}/>
-                    <WeatherInfo/>
+                    <WeatherInfo {...this.state}/>
                 </div>
             </div>                                    
         </div> 
